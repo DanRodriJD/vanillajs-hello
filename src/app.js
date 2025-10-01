bd5e660ee92f2529a41161a37f82d2e332f8b295
DanRodriJD <danrodri.davila@gmail.com>
2025-10-01 02:04:25 +0000

    proyecto carta de poker

--- src/app.js
+++ src/app.js
@@ -5,7 +5,21 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+window.onload = () => {
+  document.querySelector('.card').classList.add(generateRandomSuits());
+  document.querySelector('.number').innerHTML = generateRandomNumber();
+
+;
+
+function generateRandomNumber ()  {
+  let numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  let randomNumbers = Math.floor(Math.random() * numbers.length);
+  return numbers[randomNumbers];
+}
+function generateRandomSuits ()  {
+  let suits = ["heart", "spade", "club", "diamond"];
+  let raondomSuits = Math.floor(Math.random() * suits.length);
+  return suits[raondomSuits];
+
+}
+}
